f540bc16f4a28019f8f3ef7fe670b20f0a8dc9b6
o-l-a <59871619+o-l-a@users.noreply.github.com>
2021-12-22 17:24:59 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 $config = [];
-$config['dsn'] = 'mysql:dbname=ka46535; host=db.zut.edu.pl';
-$config['username'] = 'ka46535';
-$config['password'] = 'aCTBus3w';
+$config['dsn'] = 'mysql:dbname=nazwa; host=host';
+$config['username'] = 'nazwa';
+$config['password'] = 'tajne';
